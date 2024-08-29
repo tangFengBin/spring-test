2e0ba6328dddf4539108d998a59aa8de4b3eb4ab
fengbin.tang <extern.fengbin.tang@carthunder.com>
2024-08-29 14:42:29 +0800

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+//module org.example.app {
+//    exports org.example.app.common.exception;
+//    requires spring.boot;
+//    requires java.base;
+//
+//}
---
+++ src/main/java/org/example/app/common/config/pack-info.java
@@ -0,0 +1,5 @@
+package org.example.app.common.config;
+/*
+  @author tfb
+ * 配置类
+ */
